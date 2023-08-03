bde698ba4c7103144155be5cca1d52c71c1f49ca
Arthur Hernandez <57412723+arthurhernandezp@users.noreply.github.com>
2023-08-03 10:11:29 -0300

    Update receptor.ino

--- Projetos/ProjetoCarro/receptor.ino
+++ Projetos/ProjetoCarro/receptor.ino
@@ -3,8 +3,20 @@
 
 RH_ASK driver;
 
+// Pinos de controle do SN754410
+const int motorAForward = 2;  // Sentido horário
+const int motorABackward = 3; // Sentido anti-horário
+const int motorBForward = 4;  // Sentido horário
+const int motorBBackward = 5; // Sentido anti-horário
+
 void setup() {
   Serial.begin(115200);
+
+  // Configura os pinos como saídas para controle dos motores
+  pinMode(motorAForward, OUTPUT);
+  pinMode(motorABackward, OUTPUT);
+  pinMode(motorBForward, OUTPUT);
+  pinMode(motorBBackward, OUTPUT);
 
   if (!driver.init()) {
     Serial.println("Erro na inicialização do driver!");
@@ -21,5 +33,58 @@ void loop() {
     buf[buflen] = '\0'; // Adiciona um terminador de string ao final
 
     Serial.println((char *)buf);
+    
+    // Controla os motores baseado nas direções recebidas
+    if (strcmp((char *)buf, "Cima") == 0) {
+      moverFrente();
+    }
+    else if (strcmp((char *)buf, "Baixo") == 0) {
+      moverTras();
+    }
+    else if (strcmp((char *)buf, "Esquerda") == 0) {
+      virarEsquerda();
+    }
+    else if (strcmp((char *)buf, "Direita") == 0) {
+      virarDireita();
+    }
+    // Mais direções podem ser adicionadas conforme necessário
+    else {
+      pararMotores();
+    }
   }
+}
+
+void moverFrente() {
+  digitalWrite(motorAForward, HIGH);
+  digitalWrite(motorABackward, LOW);
+  digitalWrite(motorBForward, HIGH);
+  digitalWrite(motorBBackward, LOW);
+}
+
+void moverTras() {
+  digitalWrite(motorAForward, LOW);
+  digitalWrite(motorABackward, HIGH);
+  digitalWrite(motorBForward, LOW);
+  digitalWrite(motorBBackward, HIGH);
+}
+
+void virarEsquerda() {
+  digitalWrite(motorAForward, LOW);
+  digitalWrite(motorABackward, HIGH);
+  digitalWrite(motorBForward, HIGH);
+  digitalWrite(motorBBackward, LOW);
+}
+
+void virarDireita() {
+  digitalWrite(motorAForward, HIGH);
+  digitalWrite(motorABackward, LOW);
+  digitalWrite(motorBForward, LOW);
+  digitalWrite(motorBBackward, HIGH);
+}
+
+void pararMotores() {
+  digitalWrite(motorAForward, LOW);
+  digitalWrite(motorABackward, LOW);
+  digitalWrite(motorBForward, LOW);
+  digitalWrite(motorBBackward, LOW);
 }
